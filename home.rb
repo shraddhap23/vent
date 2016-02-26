789dfc17f7320785276769f5bda80a98b8976795
Shraddha Patel <spatelrb@gmail.com>
2016-02-26 11:55:39 -0500

    stashing to pull

--- home.rb
+++ home.rb
@@ -1,25 +1,29 @@
 require "sinatra"
 require "sinatra/activerecord"
-require "./models"
+# require "./models"
 
-set :database, "sqlite3:vent.db"
+# set :database, "sqlite3:vent.db"
 
-get "/create" do
-	User.create(name: "Shraddha",email: "spatelrb@gmail.com")
+# get "/create" do
+# 	User.create(name: "Shraddha",email: "spatelrb@gmail.com")
 
-	"New user created"
-end
+# 	"New user created"
+# end
 
-get "/all" do
-	@users = User.all
+# get "/all" do
+# 	@users = User.all
+# 	erb :index
+# end
+
+# get "/newpost" do 
+# 	# user = User.first
+# 	Post.create(body: "The bus ran late and I hate my life",user_id: "user.id")
+
+# 	"New post created"
+# end
+
+get "/home" do 
 	erb :index
-end
-
-get "/newpost" do 
-	# user = User.first
-	Post.create(body: "The bus ran late and I hate my life",user_id: "user.id")
-
-	"New post created"
 end
 
 
