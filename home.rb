df80beab81d1e8a58f73c9775e2f48b2bd3ce610
Shraddha Patel <spatelrb@gmail.com>
2016-02-29 11:40:56 -0500

    worked on profiles posts and home page

--- home.rb
+++ home.rb
@@ -1,8 +1,10 @@
 require "sinatra"
 require "sinatra/activerecord"
 require "./models"
+require "sinatra/flash"
 
 set :database, "sqlite3:vent.db"
+set :sessions, true
 
 get "/" do
   erb :index
@@ -27,7 +29,7 @@ end
 post "/signup" do
   puts params.inspect
   User.create(name: params[:name], email: params[:email], password: params[:password])
-  # this is where we put the flash message 
+  flash[:notice] = "New user has been created"
   puts "my params are" + params.inspect
   	redirect "/posts"
 end
@@ -42,13 +44,14 @@ end
 
 post "/login" do
   @user = User.where(name: params[:name]).first
-  if @user.password == params[:password]
+  if @user && @user.password == params[:password]
+  	session[:user_id] = @user.id
 		redirect "/posts"
   else
-    redirect "/login"# flash message here to say combination is not ok 
+    flash[:alert] = "Combination of email and password does not match"
+    redirect "/login"
   end 
 end
-
 
 get "/posts" do 
 	@posts = Post.all
@@ -59,3 +62,39 @@ post "/posts" do
   Post.create(user_id: current_user.id, body: params[:body])
   redirect "/posts"
 end
+
+get "/profile" do 
+  if session[:user_id] == nil
+    redirect "/"
+  end
+	erb :profile
+end
+
+post "/profile" do
+  user = current_user
+
+  user.update_attribute
+
+
+post "/acct" do
+  user = current_user
+  user.update_attribute(:name, params[:name]) if params[:name] != ""
+  user.update_attribute(:birthday, params[:birthday]) if params[:birthday] != ""
+  user.update_attribute(:location, params[:location]) if params[:location] != ""
+  user.update_attribute(:email, params[:email]) if params[:email] != ""
+  redirect "/profile"
+end
+
+post "/delete" do
+  user = current_user
+  user.destroy
+  user_posts = Post.where(user_id: user.id)
+  user_posts.each do |post|
+    post.destroy
+  end 
+  flash[:delete] = "You have succesfully deleted your account"
+  session[:user_id] = nil
+  redirect "/"
+  end
+end
+
